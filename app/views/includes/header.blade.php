811442ba06d0e169ed41a62bdf0b902b08584600
DSuhinin <suhinin.dmitriy@gmail.com>
2015-05-19 17:18:41 +0300

    *added cookie supporting for signin/signout methods

--- app/views/includes/header.blade.php
+++ app/views/includes/header.blade.php
@@ -13,7 +13,12 @@
     <nav class="header-nav header-block header-nav-right">
         <a href="/downloads" class="header-nav-link">DOWNLOADS</a>
         <a href="/documents" class="header-nav-link">DOCUMENTATION</a>
+
+        @if (!is_null($auth_token))
+        <a href="/signin" class="header-nav-link">DASHBOARD</a>
+        @else
         <a href="/signin" class="header-nav-link">SIGN IN</a>
+        @endif
 		<a href="https://www.facebook.com/VirgilSec" target="_blank" class="header-nav-link social facebook">
 			<img src="/img/social-facebook.png" alt="facebook" />
 		</a>
