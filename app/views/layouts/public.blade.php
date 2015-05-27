07a2899c41613c1bcde154881c8ad98885825d25
ebaranov <ebaranov.dev@gmail.com>
2015-05-28 01:41:47 +0300

    Responsive: contact-us;

--- app/views/layouts/public.blade.php
+++ app/views/layouts/public.blade.php
@@ -1,22 +1,26 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
-        <meta http-equiv="content-type" content="text/html; charset=utf-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>
             @section('title')@show
         </title>
         <link href='//fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="/dist/public.css" type="text/css" media="screen" charset="utf-8">
     </head>
-    <body class="page">
-        @include('includes.header')
+    <body>
+        <section class="header-block">
+            @include('includes.header')
+        </section>
 
         @yield('content')
-
-        <script src="/dist/public.js"></script>
 
         <footer class="footer container">
             @include('includes.footer')
         </footer>
+
+        <script src="/dist/public.js"></script>
     </body>
 </html>
--- app/views/pages/public/contact-us.blade.php
+++ app/views/pages/public/contact-us.blade.php
@@ -3,57 +3,65 @@
 @show
 
 @section('content')
-<div class="page contact">
-    <section class="contact-heading page-heading container">
-        <h2>Contact Us</h2>
-        <p>Looking for help or have a comment?</p>
-    </section>
+    <div class="page contact">
+        <section class="contact-heading page-heading container">
+            <h2>Contact Us</h2>
 
-    <section class="contact-contacts container">
-        <div class="contact-contacts-item">
-            <img src="/img/contacts/contact-phone.png" />
-            <p>+1-571-348-4601</p>
-        </div>
-        <div class="contact-contacts-item">
-			<a href="mailto:support@virgilsecurity.com">
-				<img src="/img/contacts/contact-print.png" />
-			</a>
-            <p><a href="mailto:support@virgilsecurity.com">support@virgilsecurity.com</a></p>
-        </div>
-        <div class="contact-contacts-item social">
-            <img src="/img/contacts/contact-social.png" />
-            <p>
-                <a class="twitter" href="https://twitter.com/VirgilSecurity" target="_blank">
-                    <img src="/img/social-twitter.png" alt="twitter" />
-                </a>
-                <a class="facebook" href="https://www.facebook.com/VirgilSec" target="_blank">
-                    <img src="/img/social-facebook.png" alt="facebook" />
-                </a>
-            </p>
-        </div>
-    </section>
+            <p>Looking for help or have a comment?</p>
+        </section>
 
-    <form class="contact-form container">
-        <div class="row">
-            <div class="col-50">
-                <div class="form-item">
-                    <input class="form-input expand" type="text" name="name" placeholder="Your Name" />
+        <section class="contact-contacts container">
+            <div class="row">
+                <div class="col-sm-16 col-xs-24 contact-contacts-item">
+                    <img src="/img/contacts/contact-phone.png"/>
+
+                    <p>+1-571-348-4601</p>
                 </div>
-                <div class="form-item">
-                    <input class="form-input expand" type="text" name="email" placeholder="Your Email" />
+                <div class="col-sm-16 col-xs-24 contact-contacts-item">
+                    <a href="mailto:support@virgilsecurity.com">
+                        <img src="/img/contacts/contact-print.png"/>
+                    </a>
+
+                    <p><a href="mailto:support@virgilsecurity.com">support@virgilsecurity.com</a></p>
                 </div>
-                <div class="form-item">
-                    <input class="form-input expand" type="text" name="product" placeholder="Product" />
+                <div class="col-sm-16 hidden-xs contact-contacts-item social">
+                    <img src="/img/contacts/contact-social.png"/>
+
+                    <p>
+                        <a class="twitter" href="https://twitter.com/VirgilSecurity" target="_blank">
+                            <img src="/img/social-twitter.png" alt="twitter"/>
+                        </a>
+                        <a class="facebook" href="https://www.facebook.com/VirgilSec" target="_blank">
+                            <img src="/img/social-facebook.png" alt="facebook"/>
+                        </a>
+                    </p>
+                </div>
+            </div>
+        </section>
+
+        <form class="contact-form container">
+            <div class="row">
+                <div class="col-xs-44 col-xs-offset-2 col-sm-21 form-item">
+                    <div class="col-xs-48 form-item">
+                        <input class="form-input expand" type="text" name="name" placeholder="Your Name"/>
+                    </div>
+                    <div class="col-xs-48 form-item">
+                        <input class="form-input expand" type="text" name="email" placeholder="Your Email"/>
+                    </div>
+                    <div class="col-xs-48 form-item">
+                        <input class="form-input expand" type="text" name="product" placeholder="Product"/>
+                    </div>
+                </div>
+                <div class="col-xs-44 col-xs-offset-2 col-sm-21 form-item textarea">
+                    <textarea name="message" class="form-input expand" placeholder="Message"></textarea>
                 </div>
             </div>
 
-            <div class="col-50">
-                <textarea name="message" class="form-input expand" placeholder="Message"></textarea>
+            <div class="row">
+                <div class="col-xs-44 col-xs-offset-2 contact-form-footer">
+                    <a href="mailto:support@virgilsecurity.com" class="btn-virgil btn-transparent contact-form-submit">SEND</a>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <a href="mailto:support@virgilsecurity.com" class="btn-virgil btn-transparent contact-form-submit">SEND</a>
-        </div>
-    </form>
-</div>
+        </form>
+    </div>
 @stop
