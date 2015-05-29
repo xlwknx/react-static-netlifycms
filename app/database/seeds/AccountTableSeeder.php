5417a3d54503d0262ab4857bdf0c47c0ffbbd0a4
DSuhinin <suhinin.dmitriy@gmail.com>
2015-05-29 17:03:02 +0300

    [VDS-2] - Added database seeding for Virgil account and all Virgil applications

---
+++ app/database/seeds/AccountTableSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+class AccountTableSeeder extends Seeder {
+
+    const VIRGIL_ACCOUNT_ID = 1;
+
+    public function run()
+    {
+        DB::table('service_account')->delete();
+
+        Account::create(array(
+            'id' => self::VIRGIL_ACCOUNT_ID,
+            'email' => 'support@virgilsecurity.com',
+            'password' => md5('password'),
+            'confirmed' => Account::ACCOUNT_CONFIRMED
+        ));
+    }
+
+}
