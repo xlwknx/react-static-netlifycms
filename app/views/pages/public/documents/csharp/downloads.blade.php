4f4c3a29e729184c70cbaecb73bfb524cdedd354
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-06-05 12:19:41 -0400

    [VDS-16] add downloads page and change code element style

--- app/views/pages/public/documents/csharp/downloads.blade.php
+++ app/views/pages/public/documents/csharp/downloads.blade.php
@@ -38,8 +38,49 @@ Virgil | Developers | C#/.NET | Downloads
 	</div>
 	<div class="container">
 		<div class="row">
-			<div class="col-md-48 dev-content">
-				<h2>Introduction</h2>
+			<div class="col-md-38 dev-content">
+				<h2>Downloads for .NET</h2>
+				<p>
+					Here you can download the latest version of Crypto and SDK libraries for .NET and Xamarin platforms. 
+					If you didn't find your platform please let as know <a href="/contact-us">here</a> . 
+				</p>
+				<h3>Windows</h3>
+				<table class="table">
+					<tr>
+						<td>Virgil Crypto Library</td>
+						<td class="text-left" >1.0.0</td>
+						<td class="text-left" >.NET 4.5</td>
+						<td class="text-left">SHA-1: 3028d14150b11bf5b4b7a049a68858c70e18cdbe</td>
+						<td class="text-right"><a href="#">Download</a></td>
+					</tr>
+					<tr>
+						<td>Virgil SDK</td>
+						<td class="text-left" >1.0.0</td>
+						<td class="text-left" >.NET 4.5</td>
+						<td class="text-left"></td>
+						<td class="text-right">Coming soon...</td>
+					</tr>
+				</table>
+				<h3>Mac OSX</h3>
+				<table class="table">
+					<tr>
+						<td>Virgil Crypto Library</td>
+						<td class="text-left" >1.0.0</td>
+						<td class="text-left" >.NET/Xamarin 4.5</td>
+						<td class="text-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+						&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+						<td class="text-right">Coming soon...</td>
+					</tr>
+					<tr>
+						<td>Virgil SDK</td>
+						<td class="text-left" >1.0.0</td>
+						<td class="text-left" >.NET/Xamarin 4.5</td>
+						<td class="text-left"></td>
+						<td class="text-right">Coming soon...</td>
+					</tr>
+				</table>
 			</div>
 		</div>
 	</div>
