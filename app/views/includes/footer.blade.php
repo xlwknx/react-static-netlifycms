c997545639e4e17f9266062695e9465a6ece9ddc
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-08-29 12:54:12 -0700

    adjust social icons styles

--- app/views/includes/footer.blade.php
+++ app/views/includes/footer.blade.php
@@ -39,8 +39,8 @@
         <div class="col-md-3">
             <p>+1-571-348-4601</p>
             <p><a href="mailto:support@virgilsecurity.com">support@virgilsecurity.com</a></p>         
-            <a class="social-img twitter" href="https://twitter.com/virgilsecurity">
-                <svg  version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" 
+            <a class="social-link" href="https://twitter.com/virgilsecurity">
+                <svg class="twitter" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" 
                     viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
                     <path id="twitter-4-icon" d="M256,50C142.229,50,50,142.229,50,256s92.229,206,206,206s206-92.229,206-206S369.771,50,256,50z
                         M359.599,220.506c3.021,67.199-47.096,142.124-135.802,142.124c-26.981,0-52.096-7.911-73.238-21.466
@@ -50,10 +50,27 @@
                         c10.884-2.143,21.108-6.119,30.341-11.594c-3.567,11.157-11.144,20.521-21.008,26.433c9.665-1.153,18.874-3.722,27.441-7.523
                         C377.035,205.355,368.935,213.771,359.599,220.506z"/>
                 </svg>
-            </a>
-            <img class="social-img" src="/img/social-twitter-icon.svg" /> 
-            <img class="social-img" src="/img/social-facebook-icon.svg" /> 
-            <img class="social-img" src="/img/social-linkedin-icon.svg" />   
+            </a> 
+            <a class="social-link" href="https://twitter.com/virgilsecurity">
+                <svg class="facebook" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"                
+                    width="512px" height="512px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">                
+                    <path id="facebook-circle-icon" d="M256.417,50c-113.771,0-206,92.229-206,206s92.229,206,206,206s206-92.229,206-206                
+                        S370.188,50,256.417,50z M317.385,171.192c0,0-20.604,0-28.789,0c-10.162,0-12.28,4.163-12.28,14.678c0,8.75,0,25.404,0,25.404                
+                        h41.069l-3.951,44.596h-37.118v133.227h-53.2V256.435h-27.666v-45.16h27.666c0,0,0-6.493,0-35.565                
+                        c0-33.379,17.849-50.807,57.437-50.807c6.484,0,36.833,0,36.833,0V171.192z"/>                
+                </svg>
+            </a>  
+            <a class="social-link" href="https://twitter.com/virgilsecurity">
+                <svg class="linkedin" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"                
+                    width="512px" height="512px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">                
+                    <path id="linkedin-circle-icon" d="M256.417,50c-113.771,0-206,92.229-206,206s92.229,206,206,206s206-92.229,206-206                
+                        S370.188,50,256.417,50z M201.456,355.592h-45.229V209.469h45.229V355.592z M178.626,190.333c-14.771,0-26.746-12.072-26.746-26.963                
+                        s11.975-26.963,26.746-26.963c14.77,0,26.745,12.072,26.745,26.963S193.396,190.333,178.626,190.333z M370.953,355.592h-45.01                
+                        c0,0,0-55.666,0-76.703s-7.991-32.781-24.626-32.781c-18.103,0-27.562,12.231-27.562,32.781c0,22.504,0,76.703,0,76.703h-43.38                
+                        V209.469h43.38v19.679c0,0,13.047-24.137,44.032-24.137c30.986,0,53.165,18.918,53.165,58.058                
+                        C370.953,302.209,370.953,355.592,370.953,355.592z"/>                
+                </svg>
+            </a>            
         </div>
     </div>
     <div class="row footer-brend">
