faae8ef211db2957cdaf48f869a4e0d1f81a368c
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-09-04 01:38:20 -0400

    change button text

--- app/views/pages/account/signin.blade.php
+++ app/views/pages/account/signin.blade.php
@@ -17,7 +17,7 @@
                 @endif
                 <form action="/signin" method="post">
                     <div class="input-group">
-                      <span class="input-group-addon" id="basic-addon1">Your Email:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span>
+                      <span class="input-group-addon" id="basic-addon1">Your Email:</span>
                       <input type="text" name="email" class="form-control" placeholder="email@example.com" aria-describedby="basic-addon1" value="<?=Input::old('email')?>">
                     </div>
                     <div class="input-group">
@@ -30,7 +30,7 @@
                                 <button type="submit" class="btn btn-primary">Sign In</button>      
                             </div>
                             <div class="col-md-6 col-sm-6">
-                                <a href="/signup" class="btn btn-default">Create an Account?</a>
+                                <a href="/signup" class="btn btn-default">Create free Account</a>
                             </div>
                         </div>
                     </div>       
