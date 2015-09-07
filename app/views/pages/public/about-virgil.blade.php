5eac5ed218d5bf54c67e4cc5ea1c2c44bb99f63a
Denis Kurilenko <kurilenkodenis@gmail.com>
2015-09-06 21:11:01 -0400

    adjust apps page and create about us page with press itens

--- app/views/pages/public/about-virgil.blade.php
+++ app/views/pages/public/about-virgil.blade.php
@@ -6,75 +6,140 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12 text-center">
-            <h1 class="about-viril-title">Virgil Security is a New Generation<br /> Security, Privacy, Encryption and PKI</h1>
-            <h4 class="about-viril-sub-title">Virgil Security provides a public key management infrastructure that dramatically<br /> eases the pain of development, deployment, and use of high-grade encryption.</h4>
-        </div>
-    </div>        
+            <h1 class="about-viril-title">THE SECURITY OF EVERYTHING</h1>
+        </div>                
+    </div>    
     <div class="row">
-        <div class="col-md-4 about-viril-item">
-            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                     viewBox="-99 -161.7 90 90" style="enable-background:new -99 -161.7 90 90;" xml:space="preserve">
-                <g>
-                        <path d="M-82.9-71.7h-12.9c-0.7,0-1.4-0.3-1.8-0.8c-0.5-0.5-0.7-1.2-0.6-1.9L-97-87c0.1-0.6,0.3-1.1,0.7-1.5l31.1-30.4
-                            c-2.1-3.9-3.1-8.3-3.1-13.6c0-16.1,13.1-29.2,29.3-29.2c16.1,0,29.3,13.1,29.3,29.2c0,16.1-13.1,29.2-29.3,29.2
-                            c-4.1,0-7.5-0.3-10.9-1.8l-6.8,6.2l-0.6,7.8c-0.1,1.3-1.1,2.3-2.4,2.3l-7,0.3l-1.5,6.7c-0.2,1-1,1.8-2,1.9l-6.3,1l-4.3,6.1
-                            C-81.3-72.1-82.1-71.7-82.9-71.7z M-93-76.6h8.9l4.2-5.9c0.4-0.5,1-0.9,1.6-1l5.6-0.9l1.6-6.9c0.3-1.1,1.2-1.9,2.3-1.9l6.7-0.3
-                            l0.5-6.6c0-0.6,0.3-1.2,0.8-1.7l8.8-8c0.8-0.7,1.9-0.8,2.8-0.4c3.2,1.6,6.1,1.9,10.3,1.9c13.4,0,24.3-10.9,24.3-24.2
-                            c0-13.4-10.9-24.2-24.3-24.2c-13.4,0-24.3,10.9-24.3,24.2c0,5.2,1,9.1,3.3,12.6c0.6,1,0.5,2.3-0.3,3.1l-31.9,31.2L-93-76.6z"/>
-                        <path d="M-32.9-130.6c-4.9,0-9-4-9-8.9c0-4.9,4-8.9,9-8.9c4.9,0,9,4,9,8.9C-23.9-134.6-27.9-130.6-32.9-130.6z M-32.9-145.9
-                            c-3.6,0-6.5,2.9-6.5,6.4c0,3.6,2.9,6.4,6.5,6.4c3.6,0,6.5-2.9,6.5-6.4S-29.3-145.9-32.9-145.9z"/>
-                </g>
-            </svg>
-            <p>
-                <b>Virgil Security</b> operates an open, global key exchange service that provisions, distributes, 
-                public keys Virgil Stack cloud Apis connect to an operated by Virgil service is a metered 
-                cloud API.
-            </p>
+        <div class="col-md-2 col-md-offset-5 col-sm-2 col-sm-offset-5 col-xs-4 col-xs-offset-4">
+            <hr class="about-hr" />            
         </div>
-        <div class="col-md-4 about-viril-item">
-            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                 viewBox="-99 -161.7 90 90" style="enable-background:new -99 -161.7 90 90;" xml:space="preserve">
-                <g>
-                    <g>
-                        <g>
-                            <path d="M-97.5-130.2l41.9,23.8c0.5,0.3,1,0.4,1.5,0.4c0.5,0,1-0.1,1.5-0.4l42.1-24.1c0.9-0.5,1.5-1.5,1.5-2.6
-                                c0-1.1-0.6-2.1-1.5-2.6l-41.9-24.6c-0.9-0.6-2.1-0.6-3.1,0l-42.1,25c-0.9,0.5-1.5,1.5-1.5,2.6C-99-131.7-98.4-130.7-97.5-130.2z
-                                 M-53.9-154.3l35.9,21.1l-36.1,20.7L-90-132.8L-53.9-154.3z M-13.5-119l-40.6,23.3l-40.4-22.9c-1.4-0.8-3.3-0.3-4.1,1.1
-                                s-0.3,3.3,1.1,4.1l41.9,23.8c0.5,0.3,1,0.4,1.5,0.4c0.5,0,1-0.1,1.5-0.4l42.1-24.1c1.4-0.8,1.9-2.7,1.1-4.1
-                                C-10.2-119.3-12.1-119.8-13.5-119z M-13.5-102.3L-54.1-79l-40.4-22.9c-1.4-0.8-3.3-0.3-4.1,1.1c-0.8,1.4-0.3,3.3,1.1,4.1
-                                l41.9,23.8c0.5,0.3,1,0.4,1.5,0.4c0.5,0,1-0.1,1.5-0.4l42.1-24.1c1.4-0.8,1.9-2.7,1.1-4.1C-10.2-102.6-12.1-103.1-13.5-102.3z"/>
-                        </g>
-                    </g>
-                </g>
-            </svg>
-            <p>
-                <b>Virgil Stack</b> is a set of open-source software libraries and cloud APIs that empower developers 
-                to deliver highly secure software applications with ubiquitous use of encryption for both data 
-                transmission and storage.
-            </p>
+    </div>    
+   <div class="row">
+        <div class="col-md-12 text-center">
+            <h1>Our Team</h1>
         </div>
-        <div class="col-md-4 about-viril-item">
-            <svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-                 viewBox="-99 -161.7 90 90" style="enable-background:new -99 -161.7 90 90;" xml:space="preserve">
-                <g>
-                    <path d="M-17-149.9c-0.5-0.4-1.1-0.7-1.7-0.6c-17.8,1.1-25.8-3.1-33.7-10.5c-0.9-0.9-2.3-0.9-3.2,0c-7.9,7.4-15.9,11.6-33.7,10.5
-                        c-0.6,0-1.3,0.2-1.7,0.6c-0.5,0.4-0.7,1-0.7,1.7v39c0,20.7,16.9,37.6,37.7,37.6c20.8,0,37.7-16.9,37.7-37.6v-39
-                        C-16.3-148.9-16.5-149.5-17-149.9z M-54-76.3c-18.2,0-33.1-14.8-33.1-32.9v-36.5c17,0.6,25.4-3.5,33.1-10.4
-                        c7.7,6.9,16.1,11,33.1,10.4v36.5C-20.9-91.1-35.8-76.3-54-76.3z"/>
-                    <path d="M-36.7-121.5l-7.3-2.2l1-5.3c0.1-0.5,0.1-2.8,0.1-2.8c0-3.1-2.5-5.6-5.6-5.6h-0.9c-1.8,0-3.2,1.4-3.3,3.1l-0.1,2.4
-                        c0,1.3-0.2,2-0.7,2.9l-2.3,5.6c-1.1,1.7-3,2.9-5,3.1c-0.7-1.1-1.9-1.8-3.2-1.8h-6.8c-2.1,0-3.8,1.7-3.8,3.8v17.4
-                        c0,2.1,1.7,3.8,3.8,3.8h6.8c1.7,0,3.2-1.3,3.6-2.9h1.6c0.5,0,1,0.2,1.4,0.7l1.2,1.3c1.1,1.2,2.6,1.9,4.2,1.9l9.7,0
-                        c2.5,0,4.8-1.7,5.5-4.1l3.9-14.3C-32.1-117.6-33.8-120.7-36.7-121.5z M-70.8-118.3l6.8,0v17.4l-6.8,0L-70.8-118.3z M-58.8-103.8
-                        h-1.4v-12.7c3.1-0.4,6-2.2,7.7-4.9l2.5-6c0.6-1.1,1.1-2.3,1-4.5l0.1-1.7h0.4c1,0,1.8,0.8,1.8,1.8c0,0,0,1.7-0.1,2l-1.4,7
-                        c-0.2,1,0.4,1.9,1.3,2.2l8.9,2.7c1,0.3,1.6,1.3,1.3,2.3h0l-4,14.3c-0.2,0.8-1,1.4-1.8,1.4l-9.7,0c-0.7,0-1.1-0.2-1.4-0.6l-1.2-1.4
-                        C-55.7-103.1-57.2-103.8-58.8-103.8z"/>
-                </g>
-            </svg>
-            <p>
-                <b>Virgil Security</b> provides easy-to-deploy and easy-to-use cryptography software and services for 
-                use by developers and end-users
-            </p>        
+     </div>  
+    <div class="row">
+        <div class="col-md-12">
+            <div class="media about-media-item">
+              <div class="media-left">
+                <a href="#">
+                  <img class="media-object" src="/img/about-us-michael.png" alt="Michael W. Wellman">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Michael W. Wellman</h4>
+                <h5>CEO and Founder</h5>
+                From early Internet software, the first two generations of Apple’s Airport, and multiple market-defining Wi-Fi products to R&D in Cognitive Radios, Dynamic Spectrum Access (DSA), mobile ad hoc networks (MANET), and MIMO, Michael has 30+ years of bringing new technologies to market. He unwinds by getting into small boats and guiding them down fast rivers. Between whitewater trips, he mentors, supports and invests in entrepreneurs.
+              </div>
+            </div>
+            <div class="media about-media-item">
+              <div class="media-left">
+                <a href="#">
+                  <img class="media-object" src="/img/about-us-dmitry.png" alt="Dmitry Dain">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Dmitry Dain</h4>
+                <h5>CTO and Founder</h5>
+                For 15+ years, Dmitry has brought new technologies to market. His past projects include multiple market-defining Wi-Fi products, foundational development in cognitive radios and Dynamic Spectrum Access (DSA) for DARPA’s XG program and building a 3.2 million user customer base as CTO of divShare and Kadoo. First and foremost, he and his wife enjoy the adventure of raising their son.  When he has a moment, Dmitry relaxes by writing code (and then writing more code).
+              </div>
+            </div>            
+            <div class="media about-media-item">
+              <div class="media-left">
+                <a href="#">
+                  <img class="media-object" src="/img/about-us-elyzabeth.png" alt="Elyzabeth Holford">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Elyzabeth Holford</h4>
+                <h5>Business Development</h5>
+                For 25+ years, Elyzabeth’s business development work has taken her to India, China, the Caribbean, and Western Europe. In the US, she has served as COO of Carina Technologies and CDO of Neutral Net, Inc.. Prior to that, she earned tenure at Virginia Tech and she started her career as a successful trial attorney. She also has led statewide and international advocacy organizations. When she’s not leading startups and turnarounds or doing tai chi or swimming, she’s been known to play guitar with a band or two.
+              </div>
+            </div>            
+            <div class="media about-media-item">
+              <div class="media-left">
+                <a href="#">
+                  <img class="media-object" src="/img/about-us-laurel.png" alt="Laurel Boylen">
+                </a>
+              </div>
+              <div class="media-body">
+                <h4 class="media-heading">Laurel Boylen</h4>
+                <h5>Developer Ecosystem</h5>
+                Laurel has built ecosystems for startups in the identity management and data privacy sectors in Honolulu, San Diego, Palo Alto, Virginia, and New York.  She brought her first customer base to 1M and, with Dmitry, brought divShare and Kadoo to 3.2M people.  While following her dream of users controlling their data, she mentors high school students for FIRST’s Robotics Challenge, rides horses, surfs, and melts metal for interactive engineering sculptures.
+              </div>
+            </div>            
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-2 col-md-offset-5 col-sm-2 col-sm-offset-5 col-xs-4 col-xs-offset-4">
+            <hr class="about-hr" />            
+        </div>
+    </div>
+    <div class="about-press">
+         <div class="row">
+            <div class="col-md-12 text-center">
+                <h1>Virgil Press</h1>
+            </div>
+         </div>  
+         <div class="row">
+             <div class="col-md-4 about-press-item">
+                <h5>September 3, 2015</h5>
+                <p>DC Download: Mach37 Alumni Bringing in Funding</p>
+                <a href="http://dcinno.streetwise.co/2015/09/03/1776-invests-in-dc-startup-hillary-clinton-on-snapchat">Read more...</a>
+             </div>
+             <div class="col-md-4 about-press-item ">
+                <h5>September 2, 2015</h5>
+                <p>Virgil Security raised $525,000 on September 2nd, 2015, from Inner Loop Capital, Nextgen...</p>
+                <a href="http://www.whogotfunded.com/deals/368657-virgil-security">Read more...</a> 
+             </div>
+             <div class="col-md-4 about-press-item ">
+                <h5>September 2, 2015</h5>
+                <p>MACH37™ Cyber Accelerator Announces New Funding for Two Program Graduates, Virgil Security...</p>
+                <a href="http://www.prweb.com/releases/2015/MACH37/prweb12939086.htm">Read more...</a>
+             </div>
+         </div>     
+         <div class="row">
+             <div class="col-md-4 about-press-item">
+                <h5>September 2, 2015</h5>
+                <p>MACH37 Announces New Funding for Virgil Security and Cyph</p>
+                <a href="http://www.darkreading.com/analytics/mach37-announces-new-funding-for-virgil-security-and-cyph/d/d-id/1322026">Read more...</a>
+             </div>
+             <div class="col-md-4 about-press-item ">
+                <h5>September 2, 2015</h5>
+                <p>Virgil Security Raises $525k in Seed Funding</p>
+                <a href="http://www.finsmes.com/2015/09/virgil-security-raises-525k-seed-funding.html">Read more...</a> 
+             </div>
+             <div class="col-md-4 about-press-item ">
+                <h5>September 2, 2015</h5>
+                <p>How the Mach37 Mafia Continues To Thrive: Graduates of the cybersecurity accelerator are...</p>
+                <a href="http://dcinno.streetwise.co/2015/09/02/cybersecurity-accelerator-mach37-alumni-startups-raise-seed">Read more...</a>
+             </div>
+         </div>  
+        <div class="row">
+             <div class="col-md-4 about-press-item">
+                <h5>August 5, 2015</h5>
+                <p>CTIA Announces CTIA Startup Lab Finalists for 2015</p>
+                <a href="http://www.ctia.org/resource-library/press-releases/archive/ctia-announces-ctia-startup-lab-finalists-for-2015">Read more...</a>
+             </div>
+             <div class="col-md-4 about-press-item ">
+                <h5>April 2, 2015</h5>
+                <p>Top Ten DC Area Startups Selected to Pitch in Arlington for 36|86’s Southern Series</p>
+                <a href="http://launchtn.org/2015/04/top-ten-startups-selected-to-pitch-in-arlington-for-3686s-southern-series">Read more...</a> 
+             </div>
+             <div class="col-md-4 about-press-item ">
+                <h5>November 11, 2014</h5>
+                <p>Venture Advisor Session With Mach37</p>
+                <a href="http://www.tandemnsi.com/venture-advisor-session-with-mach37">Read more...</a>
+             </div>
+         </div> 
+         <div class="row">
+             <div class="col-md-4 about-press-item">
+                <h5>September 1, 2014</h5>
+                <p>Virginia accelerator welcomes new cohort of cybersecurity companies</p>
+                <a href="http://www.washingtonpost.com/business/capitalbusiness/virginia-accelerator-welcomes-new-cohort-of-cybersecurity-companies/2014/09/01/df730ec4-2fbf-11e4-994d-202962a9150c_story.html">Read more...</a>
+             </div>
+         </div>  
+     </div> 
 </div>
 @stop
--- app/views/pages/public/apps.blade.php
+++ app/views/pages/public/apps.blade.php
@@ -7,92 +7,54 @@
 <div class="container">
     <div class="row">
         <div class="col-md-12 text-center" >
-            <h1 class="apps-title">Download Virgil Security <br /> Applications</h1>
+            <h1 class="apps-title">Get Started with Virgil Apps</h1>
         </div>        
     </div>
     <div class="row">
-        <div class="col-md-6">
-            <div class="apps-item">
-                <div class="media">
-                  <div class="media-left media-top">
-                    <a href="#">
-                      <img class="media-object" src="/img/virgil_pass_app_icon.png" alt="Virgil Pass">
-                    </a>
-                  </div>
-                  <div class="media-body">
-                    <h4 class="media-heading">Virgil Pass</h4>
-                    <p><b>Simple, secure passwordless authentication for websites and apps.</b></p>
-                    The Virgil Pass extension provides passwordless authentication for any site that supports Virgil Pass. 
-                    Virgil Pass is a next-generation authentication, encryption, and verification system based on the 
-                    Elliptic Curve Integrated Encryption Scheme (ECIES). Virgil Pass combines standards-based cryptography 
-                    with an open, global key management system to make state-of-the-art privacy and security available to 
-                    any application developer.<br />
-                    <a class="btn btn-default apps-donwload-btn" disabled="disabled" >Coming Soon...</a>
-                  </div>
-                </div>      
-            </div>      
+      <div class="col-sm-4 col-md-4">
+        <div class="thumbnail apps-item">
+          <img src="/img/virgil_pass_app_icon.png" alt="Virgil Pass">
+          <div class="caption">
+            <h3>Virgil Pass</h3>
+            <p>Get rid of passwords and secure your online world easily and for free. Pick and click on what you want to secure. (Chrome, Firefox, iOS, and Android)<br /><br /><br /><br /></p>
+            <p><a href="#" class="btn btn-default" disabled="disabled" role="button">Coming Soon...</a></p>
+          </div>
         </div>
-        <div class="col-md-6">
-            <div class="apps-item">
-                <div class="media-left media-top">
-                    <a href="#">
-                      <img class="media-object" src="/img/virgil_sync_app_icon.png" alt="Virgil Keys">
-                    </a>
-                </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Virgil Sync</h4>
-                    <p><b>Simple, end-to-end encrypted file sync for your documents, photos, videos and files.</b></p>
-                    Virgil Security, Inc., provides easy-to-deploy and easy-to-use cryptographic software and services for 
-                    use by developers and end-users. Virgil Security’s encryption libraries and services, along with an 
-                    accompanying public key management infrastructure, ease the pain of developing, deploying, and using 
-                    strong cryptography. Virgil Security enables a new generation of enhanced privacy and security for 
-                    applications.<br />
-                    <a class="btn btn-default apps-donwload-btn" disabled="disabled" >Coming Soon...</a>
-                </div>
-            </div>
-        </div>            
+      </div>
+
+      <div class="col-sm-4 col-md-4">
+        <div class="thumbnail apps-item">
+          <img src="/img/virgil_sync_app_icon.png" alt="Virgil Sync">
+          <div class="caption">
+            <h3>Virgil Sync</h3>
+            <p>Keep your cloud safe using Virgil. Whether you are storing or moving your documents, photos, videos and files, Virgil provides seamless integration with exceptional protection for free. (Windows, Mac, Linux)</p>
+            <p><a href="#" class="btn btn-default" disabled="disabled" role="button">Coming Soon...</a></p>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-sm-4 col-md-4">
+        <div class="thumbnail apps-item">
+          <img src="/img/virgil_mail_app_icon.png" alt="Virgil Mail">
+          <div class="caption">
+            <h3>Virgil Mail</h3>
+            <p>Click here and send secure emails today. Protect your communications with free, seamless end-to-end encryption using Virgil. (Outlook, Mac - but must mute Mac icon until it is working)<br /><br /></p>
+            <p><a href="#" class="btn btn-default" disabled="disabled" role="button">Coming Soon...</a></p>
+          </div>
+        </div>
+      </div>
+
     </div>
     <div class="row">
-        <div class="col-md-6">
-            <div class="apps-item">
-                <div class="media">
-                  <div class="media-left media-top">
-                    <a href="#">
-                      <img class="media-object" src="/img/virgil_mail_app_icon.png" alt="Virgil Pass">
-                    </a>
-                  </div>
-                  <div class="media-body">
-                    <h4 class="media-heading">Virgil Mail</h4>
-                    <p><b>Seamless end-to-end encrypted email for Outlook clients that uses Virgil Keys.</b></p>
-                    The Virgil Pass extension provides passwordless authentication for any site that supports Virgil Pass.
-                    Virgil Pass is a next-generation authentication, encryption, and verification system based on the 
-                    Elliptic Curve Integrated Encryption Scheme (ECIES). Virgil Pass combines standards-based cryptography
-                    with an open, global key management system to make state-of-the-art privacy and security available to 
-                    any application developer.<br />
-                   <a class="btn btn-default apps-donwload-btn" disabled="disabled" >Coming Soon...</a>
-                  </div>
-                </div>  
-            </div>          
+        <div class="col-md-2 col-md-offset-5 col-sm-2 col-sm-offset-5 col-xs-4 col-xs-offset-4">
+            <hr class="apps-hr" />            
         </div>
-        <div class="col-md-6">
-            <div class="apps-item">
-                <div class="media-left media-top">
-                    <a href="#">
-                      <img class="media-object" src="/img/virgil_keys_app_icon.png" alt="Virgil Sync">
-                    </a>
-                </div>
-                <div class="media-body">
-                    <h4 class="media-heading">Virgil Keys</h4>
-                    <p><b>Manage your Virgil Identity keys, as well as apps that use them.</b></p>
-                    Virgil Security, Inc., provides easy-to-deploy and easy-to-use cryptographic software and services 
-                    for use by developers and end-users. Virgil Security’s encryption libraries and services, along with 
-                    an accompanying public key management infrastructure, ease the pain of developing, deploying, and 
-                    using strong cryptography. Virgil Security enables a new generation of enhanced privacy and security 
-                    for applications, cloud services, and the Internet of Things.<br />
-                    <a class="btn btn-default apps-donwload-btn" disabled="disabled" >Coming Soon...</a>
-                </div>
-            </div>
-        </div>            
+    </div>
+    <div class="row">
+        <div class="col-md-12 apps-footer">
+            <p>Virgil Security has developed the next-generation authentication, encryption, and verification system based on the Elliptic Curve Integrated Scheme (ECIES). Our products provide enhanced security while protecting individual privacy across applications, cloud services and the Internet of Things. 
+            <p>We use standards-based cryptography with an open, global key management system to make state-of-the-art privacy and security available to everyone. Virgil Security’s encryption libraries and services, along with the accompanying public key management infrastructure, are quick and easy to implement and they work seamlessly in the background.</p>          
+        </div>
     </div>
 </div>        
 @stop
