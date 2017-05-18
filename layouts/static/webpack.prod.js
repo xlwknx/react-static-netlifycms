74848cdf55e1c3cb84674c3b3ada0d7794f2d75f
Anastasia <anastacia.chevguz@gmail.com>
2017-05-18 18:18:00 +0300

    [WWW-470] - Website migration. Home page. Static. WIP

---
+++ layouts/static/webpack.prod.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './src/index.js',
+    './src/styles/main.scss'
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'resolve-url-loader', 'sass-loader?sourceMap'])
+      },
+      {
+        test: /\.(otf|gif|jpe?g|png|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+        loader: ['file-loader?name=assets/[name].[ext]']
+        //loader: ['base64-inline-loader?limit=1000&name=assets/[name].[ext]']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      hash: true,
+      filename: 'index.html',
+      template: __dirname + '/src/index.html'
+    }),
+    new ExtractTextPlugin({
+      filename: '[name].bundle.css',
+      allChunks: true
+    })
+  ]
+};
