7787d410b99bd8dcad2c5c45b7647992e9442a9f
Yurii Kuzich <virgilkuzich@gmail.com>
2017-06-20 11:44:37 +0300

    Revert "[WP_TEMPLATE]: refactor customizer"
    This reverts commit 731548624688e8c15d742728c3ffcd56ca5b586b.

--- layouts/wp_template/header.php
+++ layouts/wp_template/header.php
@@ -1,4 +1,9 @@
 <?php require_once '_header.php' ?>
 <header class="header">
-  <?php get_template_part('template-sections/header/header_section'); ?>
+    <div class="header-logo">
+        <a href="<?= home_url() ?>">
+            <img src="<?php echo get_theme_file_uri('assets/logo.png') ?>" alt="">
+        </a>
+    </div>
+    <?php dynamic_sidebar('top-menu') ?>
 </header>
