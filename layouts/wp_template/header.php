2c249764e89434679941fbbc0ad782c565edbd44
Yurii Kuzich <virgilkuzich@gmail.com>
2017-06-20 22:20:50 +0300

    [WP_TEMPLATE]: fixed fav icon

--- layouts/wp_template/header.php
+++ layouts/wp_template/header.php
@@ -5,9 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
     <? if (!has_site_icon()): ?>
-        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png">
-        <link rel="shortcut icon" href="favicons/favicon.ico">
+        <link rel="icon" type="image/png" sizes="32x32" href="<?= get_theme_file_uri('favicons/favicon-32x32.png') ?>">
+        <link rel="icon" type="image/png" sizes="16x16" href="<?= get_theme_file_uri('favicons/favicon-16x16.png') ?>">
+        <link rel="shortcut icon" href="<?= get_theme_file_uri('favicons/favicon.ico') ?>">
     <? endif; ?>
 
     <?php wp_head(); ?>
