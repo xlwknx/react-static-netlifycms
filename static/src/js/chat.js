2d70be1070e82b48a5ffc814c53e955d0788e5aa
Alexey Smirnov <alexey@smirnov.works>
2018-01-29 18:00:40 +0200

    working steps with images and animations

--- static/src/js/chat.js
+++ static/src/js/chat.js
@@ -1,10 +1,88 @@
 function initTabs() {
+
+    var interval1, interval2, interval3;
+
+    function startScene1() {
+        endScene2();
+        endScene3();
+
+        var el = $('.step1-loading').removeClass('hidden');
+
+        function loading() {
+            var text = $('.step1-loading').text();
+            text = text.replace(/\.+/, (match) => match.length < 3 ? match + '.' : '.');
+            $('.step1-loading').text(text);
+        }
+        
+        interval1 = setInterval(loading, 500);
+    }
+
+    function startScene2() {
+        endScene1();
+        endScene3();
+
+        $('.step2-scene').removeClass('hidden');
+        var message = 'Hey Bob, are you crazy?';
+        var currentMessage = '';
+        
+        interval2 = setInterval(startTyping, 200);
+
+        function startTyping() {
+            var currentChar = currentMessage.length;
+            if (currentChar >= message.length - 1) {
+                clearInterval(interval2);
+                $('#step2-message').removeClass('hidden');
+                $('.step2-sending-message').addClass('hidden');
+            }
+            currentMessage = currentMessage + message[currentChar];
+            $('.step2-sending-message').text(currentMessage);
+        }
+    }
+
+    function startScene3() {
+        endScene1();
+        endScene2();
+        interval3 = setInterval(() => $('#step3-message').removeClass('hidden'), 2000)
+    }
+
+    function endScene1() {
+        $('.step1-loading').addClass('hidden');
+        clearInterval(interval1);
+    }
+
+    function endScene2() {
+        $('.step2-scene').addClass('hidden');
+        $('#step2-message').addClass('hidden');
+        $('.step2-sending-message').removeClass('hidden');
+        $('.step2-sending-message').text('');
+        clearInterval(interval2);
+    }
+
+    function endScene3() {
+        clearInterval(interval3);
+        $('#step3-message').addClass('hidden');
+    }
+
     $(document).ready(function(){
+        startScene1();
+        
         $('#step-tabs .stepControl').click(function(){
             var tab_id = $(this).attr('data-tab');
 
             $('.stepContent').removeClass('current');
             $('.stepControl').removeClass('current');
+
+            switch (tab_id) {
+                case 'step1':
+                    startScene1();
+                    break;
+                case 'step2':
+                    startScene2();
+                    break;
+                case 'step3':
+                    startScene3();
+                    break;
+            }
 
             if (tab_id === 'step3') {
                 $('.iphone1').addClass('slideOut').removeClass('slideIn');
