de502136885a80289b497d47127a7598e168aea4
Alexey Smirnov <alexey@smirnov.works>
2018-02-01 17:56:43 +0200

    chat fix and pricing landing page

--- static/src/js/chat.js
+++ static/src/js/chat.js
@@ -1,6 +1,6 @@
 function initTabs() {
 
-    var interval1, interval2, interval3;
+    var interval1, interval2, interval3, interval31;
 
     function startScene1() {
         endScene2();
@@ -23,7 +23,7 @@ function initTabs() {
         endScene1();
         endScene3();
         $('.step2-scene').removeClass('hidden');
-        var message = 'Woodchuck can\'t chuck wood';
+        var message = 'Woodchuck can\'t chuck wood.';
         var currentMessage = '';
         
         interval2 = setInterval(startTyping, 100);
@@ -46,9 +46,13 @@ function initTabs() {
         $('.step2-scene').removeClass('hidden');
         $('#step2-message').removeClass('hidden');
         
-        interval3 = setInterval(function() {
+        interval3 = setTimeout(function() {
             $('#step3-message').removeClass('hidden')
         }, 2000);
+
+        interval31 = setTimeout(function() {
+            $('#step3 .button-raisedRed').removeClass('hidden');
+        }, 3000)
     }
 
     function endScene1() {
@@ -67,16 +71,20 @@ function initTabs() {
 
     function endScene3() {
         clearInterval(interval3);
+        clearInterval(interval31);
         $('.step2-scene').addClass('hidden');
         $('#step2-message').addClass('hidden');
         $('#step3-message').addClass('hidden');
+        $('#step3 .button-raisedRed').addClass('hidden');
+        
     }
 
     $(document).ready(function(){
         startScene1();
-        
+        var prev_tab_id = 'step1';
         $('#step-tabs .stepControl').click(function(){
             var tab_id = $(this).attr('data-tab');
+            if (tab_id === prev_tab_id) { return null; }
 
             $('.stepContent').removeClass('current');
             $('.stepControl').removeClass('current');
@@ -103,6 +111,7 @@ function initTabs() {
     
             $(this).addClass('current');
             $("#" + tab_id).addClass('current');
+            prev_tab_id = tab_id;
         })
     })
 }
