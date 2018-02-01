cf05a46b6bcd54d966d482744f40910049ec15cd
Yurii Kuzich <virgilkuzich@gmail.com>
2018-02-01 19:03:48 +0200

    upd pricing, chat pages

---
+++ wp_template/migrations/1517503497.php
@@ -0,0 +1,3 @@
+<?php
+set_theme_mod('pricing_page_intro_headline', 'We provide simple pricing that scales with you. Choose the features you need.');
+set_theme_mod('pricing_page_intro_text', 'Virgil Keys Service is Free During Beta');
